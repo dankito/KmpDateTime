8b8c82520edc8e0de35f5b9881d1bc3518ae1cab
dankito <git@dankito.net>
2025-04-19 00:21:41 +0200

    Added Month

--- kmp-date-time/src/commonMain/kotlin/net/dankito/kotlin/datetime/LocalDate.kt
+++ kmp-date-time/src/commonMain/kotlin/net/dankito/kotlin/datetime/LocalDate.kt
@@ -2,13 +2,20 @@ package net.dankito.kotlin.datetime
 
 data class LocalDate(
     val year: Int,
-    val month: Int,
+    val monthNumber: Int,
     val dayOfMonth: Int
 ) {
+
+    constructor(year: Int, month: Month, dayOfMonth: Int) : this(year, month.number, dayOfMonth)
+
     init {
-        require(month in 1..12) { "Invalid month, value must be in bounds [1-12]: $month" }
+        require(monthNumber in 1..12) { "Invalid month, value must be in bounds [1-12]: $monthNumber" }
         require(dayOfMonth in 1..31) { "Invalid day, value must be in bounds [1-31]: $dayOfMonth" }
     }
 
-    override fun toString() = "$year-$month-$dayOfMonth"
+
+    val month: Month by lazy { Month.forNumber(monthNumber) }
+
+
+    override fun toString() = "$year-$monthNumber-$dayOfMonth"
 }
