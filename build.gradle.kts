b1efa7b92986944f9ddfaea53e25268209646a74
dankito <git@dankito.net>
2025-04-23 21:02:52 +0200

    Released version 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 allprojects {
     group = "net.dankito.datetime"
-    version = "1.0.1-SNAPSHOT"
+    version = "1.1.0"
 
 
     ext["sourceCodeRepositoryBaseUrl"] = "github.com/dankito/KmpDateTime"
