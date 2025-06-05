3f29bf79614f3a05e4663522773e6ab213f2ac42
dankito <git@dankito.net>
2025-06-05 19:33:07 +0200

    Bumped version to 2.1.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 allprojects {
     group = "net.dankito.datetime"
-    version = "2.1.0"
+    version = "2.1.1-SNAPSHOT"
 
 
     ext["sourceCodeRepositoryBaseUrl"] = "github.com/dankito/KmpDateTime"
